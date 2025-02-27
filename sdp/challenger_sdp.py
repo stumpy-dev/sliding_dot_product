ea9571da1735bbe83253955325dd6ee46d5146ab
Sean Law <seanmylaw@gmail.com>
2025-02-27 10:37:31 -0500

    CHALLENGER njit parallel numpy dot

--- sdp/challenger_sdp.py
+++ sdp/challenger_sdp.py
@@ -1,14 +1,18 @@
 import numpy as np
+from numba import njit, prange
 
 
 def setup(Q, T):
+    sliding_dot_product(Q, T)
     return
 
 
+@njit(fastmath=True,parallel=True)
 def sliding_dot_product(Q, T):
-    m = len(Q)
+    m = Q.shape[0]
     l = T.shape[0] - m + 1
     out = np.empty(l)
-    for i in range(l):
+    for i in prange(l):
         out[i] = np.dot(Q, T[i : i + m])
+
     return out
