019023b77b0452d2f288287ab90e1eef161a9790
Sean Law <seanmylaw@gmail.com>
2025-02-28 22:31:41 -0500

    Minor change

--- merge.sh
+++ merge.sh
@@ -2,7 +2,14 @@
 
 # Merge main into all challenger branches
 
+if [[ `git branch | grep '*' | grep main | wc -l` -eq "0" ]]; then
+    echo "Error: Please checkout the main branch"
+    exit 1
+fi
+
 for BRANCH in $(ls .git/refs/heads);
   do git checkout $BRANCH ; 
   git merge origin/main $BRANCH ; 
 done
+
+git checkout main
