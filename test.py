c0008cac439f298ed1b61ca399eae92cc2cdabc8
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-03-05 18:45:29 -0500

    enhance test functions

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def naive_sliding_dot_product(Q, T):
     return out
 
 
-def test_modules():
+def test_sdp_power2():
     pmin = 3
     pmax = 13
 
@@ -24,9 +24,57 @@ def test_modules():
                 for p in range(q, pmax + 1):
                     Q = np.random.rand(2**q)
                     T = np.random.rand(2**p)
+
                     ref = naive_sliding_dot_product(Q, T)
                     comp = mod.sliding_dot_product(Q, T)
                     npt.assert_allclose(comp, ref)
+
+        except Exception as e:
+            print(f"Error in {mod.__name__}: {str(e)}")
+            raise e
+
+    return
+
+
+def test_sdp_power2_plus1():
+    pmin = 3
+    pmax = 13
+
+    modules = utils.import_sdp_mods()
+    for mod in modules:
+        try:
+            for q in range(pmin, pmax + 1):
+                for p in range(q, pmax + 1):
+                    Q = np.random.rand(2**q + 1)
+                    T = np.random.rand(2**p + 1)
+
+                    ref = naive_sliding_dot_product(Q, T)
+                    comp = mod.sliding_dot_product(Q, T)
+                    npt.assert_allclose(comp, ref)
+
+        except Exception as e:
+            print(f"Error in {mod.__name__}, with q={q} and p={p}")
+            raise e
+
+    return
+
+
+def test_sdp_power2_minus1():
+    pmin = 3
+    pmax = 13
+
+    modules = utils.import_sdp_mods()
+    for mod in modules:
+        try:
+            for q in range(pmin, pmax + 1):
+                for p in range(q, pmax + 1):
+                    Q = np.random.rand(2**q - 1)
+                    T = np.random.rand(2**p - 1)
+
+                    ref = naive_sliding_dot_product(Q, T)
+                    comp = mod.sliding_dot_product(Q, T)
+                    npt.assert_allclose(comp, ref)
+
         except Exception as e:
             print(f"Error in {mod.__name__}: {str(e)}")
             raise e
