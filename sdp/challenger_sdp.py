f7079d3c4f8a49aa6a08e08e60ff8e989051013a
Sean Law <seanmylaw@gmail.com>
2025-03-04 22:21:10 -0500

    Interact directly with pocketfft

--- sdp/challenger_sdp.py
+++ sdp/challenger_sdp.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.fft import next_fast_len
+from scipy.fft._pocketfft.basic import r2c, c2r
 
 
 def setup(Q, T):
@@ -6,9 +8,14 @@ def setup(Q, T):
 
 
 def sliding_dot_product(Q, T):
+    n = len(T)
     m = len(Q)
-    l = T.shape[0] - m + 1
-    out = np.empty(l)
-    for i in range(l):
-        out[i] = np.dot(Q, T[i : i + m])
-    return out
+    shape = next_fast_len(n)
+
+    tmp = np.empty((2, shape))
+    tmp[0, :m] = Q[::-1]
+    tmp[0, m:] = 0.0
+    tmp[1, :] = T
+    fft_2d = r2c(True, tmp, axis=-1)
+    
+    return c2r(False, np.multiply(fft_2d[0], fft_2d[1]))[m - 1:n]
