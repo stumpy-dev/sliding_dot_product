6fedc8d89070a3afb27239b5a397d249b3556d9d
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-03-09 13:54:48 -0400

    Removed flag verbose

--- python_version.sh
+++ python_version.sh
@@ -12,7 +12,7 @@ check_errs()
 
 get_safe_python_version()
 {
-    SAFE_PYTHON=$(curl -v --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
+    SAFE_PYTHON=$(curl --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
     check_errs $?
 }
 
