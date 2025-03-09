ac760e33fe98aa8ca7a4c2962cf76e09ff8bdd14
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-03-09 12:54:53 -0400

    Revert change in github action and update python_version to make it more verbose

--- python_version.sh
+++ python_version.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+check_errs()
+{
+  # Function. Parameter 1 is the return code
+  if [[ $1 -ne "0" && $1 -ne "5" ]]; then
+    echo "Error: Test execution encountered exit code $1"
+    # as a bonus, make our script exit with the right error code.
+    exit $1
+  fi
+}
+
 get_safe_python_version()
 {
-    SAFE_PYTHON=$(curl --no-progress-meter --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
+    SAFE_PYTHON=$(curl -v --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
+    check_errs $?
 }
 
 get_safe_python_version
