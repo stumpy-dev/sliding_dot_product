8e90ad54ebb8214d5c30c04b6c46feae9e5cbe53
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-03-08 21:43:56 -0500

    clean up output

--- python_version.sh
+++ python_version.sh
@@ -2,7 +2,7 @@
 
 get_safe_python_version()
 {
-    SAFE_PYTHON=$(curl --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2>/dev/null)
+    SAFE_PYTHON=$(curl --no-progress-meter --location https://devguide.python.org/versions | xmllint --html --xpath '//section[@id="supported-versions"]//table/tbody/tr[count(//section[@id="supported-versions"]//table/tbody/tr[td[.="security"]]/preceding-sibling::*)]/td[1]/p/text()' - 2> /dev/null)
 }
 
 get_safe_python_version
