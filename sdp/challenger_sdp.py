ca0b5c13e305b64d189b02c74dfa5b791b606bd7
Sean Law <seanmylaw@gmail.com>
2025-03-08 22:13:00 -0500

    Updated challenger name

--- sdp/challenger_sdp.py
+++ sdp/challenger_sdp.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy.fft import next_fast_len
-from scipy.fft._pocketfft.basic import r2c, c2r
 
 
 def setup(Q, T):
@@ -8,15 +6,9 @@ def setup(Q, T):
 
 
 def sliding_dot_product(Q, T):
-    n = len(T)
     m = len(Q)
-    shape = next_fast_len(n, real=True)
-
-    tmp = np.empty((2, shape))
-    tmp[0, :m] = Q[::-1]
-    tmp[0, m:] = 0.0
-    tmp[1, :n] = T
-    tmp[1, n:] = 0.0
-    fft_2d = r2c(True, tmp, axis=-1)
-    
-    return c2r(False, np.multiply(fft_2d[0], fft_2d[1]))[m - 1:n]
+    l = T.shape[0] - m + 1
+    out = np.empty(l)
+    for i in range(l):
+        out[i] = np.dot(Q, T[i : i + m])
+    return out
---
+++ sdp/pocketfft_r2c_c2r_sdp.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.fft import next_fast_len
+from scipy.fft._pocketfft.basic import r2c, c2r
+
+
+def setup(Q, T):
+    return
+
+
+def sliding_dot_product(Q, T):
+    n = len(T)
+    m = len(Q)
+    shape = next_fast_len(n, real=True)
+
+    tmp = np.empty((2, shape))
+    tmp[0, :m] = Q[::-1]
+    tmp[0, m:] = 0.0
+    tmp[1, :n] = T
+    tmp[1, n:] = 0.0
+    fft_2d = r2c(True, tmp, axis=-1)
+
+    return c2r(False, np.multiply(fft_2d[0], fft_2d[1]), n=shape)[m - 1 : n]
