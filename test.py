94f3835133db9d619864b98dcb93273a65f824bb
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-03-11 09:18:04 -0400

    fixed black format

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ from numpy import testing as npt
 from operator import eq, lt
 from scipy.fft import next_fast_len
 
-# README 
-# Real FFT algorithm performs more efficiently when the length 
-# of the input array `arr` is composed of small prime factors. 
-# The next_fast_len(arr, real=True) function from Scipy returns 
-# the same length if len(arr) is composed of a subset of 
-# prime numbers 2, 3, 5. Therefore, these radices are 
+# README
+# Real FFT algorithm performs more efficiently when the length
+# of the input array `arr` is composed of small prime factors.
+# The next_fast_len(arr, real=True) function from Scipy returns
+# the same length if len(arr) is composed of a subset of
+# prime numbers 2, 3, 5. Therefore, these radices are
 # considered as the most efficient for the real FFT algorithm.
 
 # To ensure that the tests cover different cases, the following cases
@@ -25,20 +25,32 @@ from scipy.fft import next_fast_len
 # Therefore:
 # 1. len(T) is composed of 2 and a subset of {3, 5}
 # 2. len(T) is composed of a subset of {3, 5}
-# 3. len(T) is composed of a subset of {7, 11, 13, ...} and 2 
+# 3. len(T) is composed of a subset of {7, 11, 13, ...} and 2
 # 4. len(T) is composed of a subset of {7, 11, 13, ...}
 # 5. len(T) is power of 2
 
-# In some cases, the prime factors are powered to a certain degree 
-# to increase the length of array to be around 1000-2000. This 
+# In some cases, the prime factors are powered to a certain degree
+# to increase the length of array to be around 1000-2000. This
 # can just let us test sliding_dot_product for wider range of
 # query lengths.
 
 test_inputs = [
-    (2 * (3**2) * (5**3), 0, eq),  # = 2250, Even `len(T)`, and `len(T) == next_fast_len(len(T))`
-    ((3**2) * (5**3), 1, eq),  # = 1125, Odd `len(T)`, and `len(T) == next_fast_len(len(T))`. 
-    (2 * 7 * 11 * 13, 0, lt),  # = 2002, Even `len(T)`, and `len(T) < next_fast_len(len(T))`
-    (7 * 11 * 13, 1, lt)  # = 1001, Odd `len(T)`, and `len(T) < next_fast_len(len(T))`
+    (
+        2 * (3**2) * (5**3),
+        0,
+        eq,
+    ),  # = 2250, Even `len(T)`, and `len(T) == next_fast_len(len(T))`
+    (
+        (3**2) * (5**3),
+        1,
+        eq,
+    ),  # = 1125, Odd `len(T)`, and `len(T) == next_fast_len(len(T))`.
+    (
+        2 * 7 * 11 * 13,
+        0,
+        lt,
+    ),  # = 2002, Even `len(T)`, and `len(T) < next_fast_len(len(T))`
+    (7 * 11 * 13, 1, lt),  # = 1001, Odd `len(T)`, and `len(T) < next_fast_len(len(T))`
 ]
 
 
@@ -60,7 +72,6 @@ def test_remainder(n_T, remainder):
 def test_comparator(n_T, comparator):
     shape = next_fast_len(n_T)
     assert comparator(n_T, shape)
-
 
 
 @pytest.mark.parametrize("n_T, remainder, comparator", test_inputs)
