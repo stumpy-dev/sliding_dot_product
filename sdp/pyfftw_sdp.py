c79ea81ac2acefb1c4b13b4102537995d321e082
NimaSarajpoor <nimasarajpoor@gmail.com>
2025-12-19 15:55:49 -0500

    addressed comments

--- sdp/pyfftw_sdp.py
+++ sdp/pyfftw_sdp.py
@@ -20,15 +20,38 @@ class SLIDING_DOT_PRODUCT:
         self.real_arr = pyfftw.empty_aligned(max_n, dtype="float64")
         self.complex_arr = pyfftw.empty_aligned(1 + (max_n // 2), dtype="complex128")
 
-        # Store FFTW objects in a dict keyed by `next_fast_n`, where n=len(T)
+        # Store FFTW objects, keyed by (next_fast_n, n_threads, planning_flag)
         self.rfft_objects = {}
         self.irfft_objects = {}
 
-    def __call__(self, Q, T, threads=1, planning_flag="FFTW_MEASURE"):
+    def __call__(self, Q, T, n_threads=1, planning_flag="FFTW_MEASURE"):
+        """
+        Parameters
+        ----------
+        Q : numpy.ndarray
+            Query array or subsequence
+
+        T : numpy.ndarray
+            Time series or sequence
+
+        n_threads : int, default=1
+            Number of threads to use for FFTW computations.
+
+        planning_flag : str, default="FFTW_MEASURE"
+            The planning flag that will be used in FFTW for planning.
+            See pyfftw documentation for details. Current options include:
+            "FFTW_ESTIMATE", "FFTW_MEASURE", "FFTW_PATIENT", and "FFTW_EXHAUSTIVE".
+
+        Returns
+        -------
+        out : numpy.ndarray
+            Sliding dot product between `Q` and `T`.
+        """
         m = Q.shape[0]
         if self.n != T.shape[0]:
             self.n = T.shape[0]
             self.next_fast_n = pyfftw.next_fast_len(self.n)
+        key = (self.next_fast_n, n_threads, planning_flag)
 
         # Update preallocated arrays if needed
         if self.next_fast_n > len(self.real_arr):
@@ -41,29 +64,29 @@ class SLIDING_DOT_PRODUCT:
         complex_arr = self.complex_arr[: 1 + (self.next_fast_n // 2)]
 
         # Get or create FFTW objects
-        rfft_obj = self.rfft_objects.get(self.next_fast_n, None)
+        rfft_obj = self.rfft_objects.get(key, None)
         if rfft_obj is None:
             rfft_obj = pyfftw.FFTW(
                 input_array=real_arr,
                 output_array=complex_arr,
                 direction="FFTW_FORWARD",
                 flags=(planning_flag,),
-                threads=threads,
+                threads=n_threads,
             )
-            self.rfft_objects[self.next_fast_n] = rfft_obj
+            self.rfft_objects[key] = rfft_obj
         else:
             rfft_obj.update_arrays(real_arr, complex_arr)
 
-        irfft_obj = self.irfft_objects.get(self.next_fast_n, None)
+        irfft_obj = self.irfft_objects.get(key, None)
         if irfft_obj is None:
             irfft_obj = pyfftw.FFTW(
                 input_array=complex_arr,
                 output_array=real_arr,
                 direction="FFTW_BACKWARD",
                 flags=(planning_flag, "FFTW_DESTROY_INPUT"),
-                threads=threads,
+                threads=n_threads,
             )
-            self.irfft_objects[self.next_fast_n] = irfft_obj
+            self.irfft_objects[key] = irfft_obj
         else:
             irfft_obj.update_arrays(complex_arr, real_arr)
 
@@ -93,10 +116,10 @@ class SLIDING_DOT_PRODUCT:
 _sliding_dot_product = SLIDING_DOT_PRODUCT(max_n=2**20)
 
 
-def setup(Q, T, threads=1, planning_flag="FFTW_MEASURE"):
-    _sliding_dot_product(Q, T, threads=threads, planning_flag=planning_flag)
+def setup(Q, T, n_threads=1, planning_flag="FFTW_MEASURE"):
+    _sliding_dot_product(Q, T, n_threads=n_threads, planning_flag=planning_flag)
     return
 
 
-def sliding_dot_product(Q, T, threads=1, planning_flag="FFTW_MEASURE"):
-    return _sliding_dot_product(Q, T, threads=threads, planning_flag=planning_flag)
+def sliding_dot_product(Q, T, n_threads=1, planning_flag="FFTW_MEASURE"):
+    return _sliding_dot_product(Q, T, n_threads=n_threads, planning_flag=planning_flag)
