a705c14e22bdec03e497074b26d3051057c96534
Nima Sarajpoor <nimasarajpoor@gmail.com>
2026-01-05 22:01:52 -0500

    Change variable name from `shape` to `next_fast_n` (#33)
    * minor change
    
    * revise variable name

--- sdp/numpy_fft_sdp.py
+++ sdp/numpy_fft_sdp.py
@@ -9,13 +9,13 @@ def setup(Q, T):
 def sliding_dot_product(Q, T, order="F"):
     n = len(T)
     m = len(Q)
-    shape = next_fast_len(n, real=True)
+    next_fast_n = next_fast_len(n, real=True)
 
-    tmp = np.empty((2, shape), order=order)
+    tmp = np.empty((2, next_fast_n), order=order)
     tmp[0, :m] = Q[::-1]
     tmp[0, m:] = 0.0
     tmp[1, :n] = T
     tmp[1, n:] = 0.0
     fft_2d = np.fft.rfft(tmp, axis=-1)
 
-    return np.fft.irfft(np.multiply(fft_2d[0], fft_2d[1]), n=shape)[m - 1 : n]
+    return np.fft.irfft(np.multiply(fft_2d[0], fft_2d[1]), n=next_fast_n)[m - 1 : n]
