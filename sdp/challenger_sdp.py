be0035e2040003b3e36edc0ebd0db76b90ad9e47
NimaSarajpoor <nimasarajpoor@gmail.com>
2026-01-08 00:22:11 -0500

    update code logic

--- sdp/challenger_sdp.py
+++ sdp/challenger_sdp.py
@@ -1,7 +1,8 @@
 import math
 
 import numpy as np
-import scipy._lib.array_api_extra as xpx
+
+# import scipy._lib.array_api_extra as xpx
 
 from scipy.special import lambertw
 from scipy.fft import next_fast_len
@@ -31,9 +32,9 @@ def _rfft_irfft_r2c2rn(Q_2D, T_2D, shape):
 
     return c2rn(
         False,
-        r2cn(True, T_2D, shape, axis=1) * r2cn(True, Q_2D, shape, axis=1),
-        n=shape,
-        axis=1,
+        r2cn(True, T_2D, shape, axes=1) * r2cn(True, Q_2D, shape, axes=1),
+        s=shape,
+        axes=1,
     )
 
 
@@ -81,63 +82,36 @@ def _sliding_dot_product(T, Q):
     shape_final = n + m - 1
     shape_final_lst = [shape_final]
     block_size, overlaps, T_step, Q_step = _calc_oa_lens(n, m)
-
-    # block_size_tuple = (block_size,)
-    # overlaps_tuple = (overlaps,)
-    # T_step_tuple = (T_step,)
-    # Q_step_tuple = (Q_step,)
-
     if T_step == n and Q_step == m:
         return _sliding_dot_product_r2c2r(Q, T)
 
-    # flip Q:
     Q = Q[::-1]
-
     curnstep1 = math.ceil((n + 1) / T_step)
     if (block_size - overlaps) * curnstep1 < shape_final:
         curnstep1 += 1
 
     curpad1 = curnstep1 * T_step - n
-
-    # nsteps1 = [curnstep1]
-    # nsteps2 = [1]
-    pad_size1 = [(0, curpad1)]
-    # pad_size2 = [(0, 0)]
-
-    if not all(curpad == (0, 0) for curpad in pad_size1):
-        T = xpx.pad(T, pad_size1, mode="constant", constant_values=0, xp=xp)
-
-    # Reshape the overlap-add parts to input block sizes.
-    split_axes = [0]
-    fft_axes = [1]
-
-    # reshape_size1 = [curnstep1, T_step]  # reshape_size1.insert(0, curnstep1)
-    # reshape_size2 = [1, Q_step]  # reshape_size2.insert(0, 1)
+    if curpad1 != 0:
+        T = np.pad(T, [(0, curpad1)], mode="constant", constant_values=0)
 
     T = xp.reshape(T, (curnstep1, T_step))
     Q = xp.reshape(Q, (1, Q_step))
+    ret = _rfft_irfft_r2c2rn(T, Q, block_size)
 
-    # fft_shape = [block_size]
-    ret = _freq_domain_conv(T, Q, block_size)
-
-    # Do the overlap-add.
-    # ax = 0
-    ax_fft = 1
-    ax_split = 0
-
+    # overlap-add
     overlap = overlaps
     if overlap is not None:
         ret, overpart = _split(ret, [-overlap], 1, xp=xp)
-        overpart = _split(overpart, [-1], ax_split, xp=xp)[0]
-        ret_overpart = _split(ret, [overlap], ax_fft, xp=xp)[0]
-        ret_overpart = _split(ret_overpart, [1], ax_split, xp)[1]
+        overpart = _split(overpart, [-1], 0, xp=xp)[0]
+        ret_overpart = _split(ret, [overlap], 1, xp=xp)[0]
+        ret_overpart = _split(ret_overpart, [1], 0, xp)[1]
         ret_overpart += overpart
 
     # Reshape back to the correct dimensionality.
     shape_ret = [
-        ret.shape[i] if i not in fft_axes else ret.shape[i] * ret.shape[i - 1]
+        ret.shape[i] if i not in [1] else ret.shape[i] * ret.shape[i - 1]
         for i in range(ret.ndim)
-        if i not in split_axes
+        if i not in [0]
     ]
     ret = xp.reshape(ret, shape_ret)
 
