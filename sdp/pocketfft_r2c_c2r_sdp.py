dddb708b77215600500a917c4cefaca1ff361cdc
NimaSarajpoor <nimasarajpoor@gmail.com>
2026-01-11 14:53:53 -0500

    minor change to help with future refactoring

--- sdp/pocketfft_r2c_c2r_sdp.py
+++ sdp/pocketfft_r2c_c2r_sdp.py
@@ -3,20 +3,24 @@ from scipy.fft import next_fast_len
 from scipy.fft._pocketfft.basic import r2c, c2r
 
 
-def setup(Q, T):
-    return
-
-
-def sliding_dot_product(Q, T):
+def _pocketfft_convolve(Q, T):
     n = len(T)
     m = len(Q)
     next_fast_n = next_fast_len(n, real=True)
 
     tmp = np.empty((2, next_fast_n))
-    tmp[0, :m] = Q[::-1]
+    tmp[0, :m] = Q
     tmp[0, m:] = 0.0
     tmp[1, :n] = T
     tmp[1, n:] = 0.0
     fft_2d = r2c(True, tmp, axis=-1)
 
-    return c2r(False, np.multiply(fft_2d[0], fft_2d[1]), n=next_fast_n)[m - 1 : n]
+    return c2r(False, np.multiply(fft_2d[0], fft_2d[1]), n=next_fast_n)
+
+
+def setup(Q, T):
+    return
+
+
+def sliding_dot_product(Q, T):
+    return _pocketfft_convolve(Q[::-1], T)[len(Q) - 1 : len(T)]
